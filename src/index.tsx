f90fa9309a29d42859a75e5124c1d7c5adc369d4
Kenny Ek <ekkenny@hotmail.com>
2020-03-21 21:59:42 +0100

    Clean up and new deps
    Add deps react-router-dom & MUI.
    
    Add Wrap App in BrowserRouter.
    
    Remove boilerplate files.
    
    Update structure of files.
    
    Update public files.

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { store } from './state/store';
+import App from './view/layouts/App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
